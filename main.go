3600605d0c62b9d4d7a4690cac73caef5bfa101f
Ngô Quang Triệu <46420237+million17@users.noreply.github.com>
2021-06-05 09:22:47 +0700

    Update main.go

--- main.go
+++ main.go
@@ -16,10 +16,7 @@ type Response struct {
 			Quote      string   `json:"quote"`
 			Length     string   `json:"length"`
 			Author     string   `json:"author"`
-			Tags   struct {
-				Num0 string `json:"0"`
-				Num2 string `json:"2"`
-			} `json:"tags"`
+			Tags       []string `json:"tags"`
 			Category   string   `json:"category"`
 			Language   string   `json:"language"`
 			Date       string   `json:"date"`
